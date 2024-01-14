0cfce12825eb1b886647096f27fd330df75d2146
AisultanAskarov <36818367+AisultanAskarov@users.noreply.github.com>
2024-01-14 18:42:17 +0300

    Initial Commit

---
+++ delivery-app-main-screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delivery-app-main-screen
+//
+//  Created by Aisultan Askarov on 14.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
